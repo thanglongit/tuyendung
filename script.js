de021ae6ec7e5aed53b3f9f54672ba6c3e5d27d9
Thăng Long IT <60378197+thanglongit@users.noreply.github.com>
2021-03-05 14:46:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const fx = new TextScramble(el);
 let counter = 0;
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 800);
+    setTimeout(next, 1888);
   });
   counter = (counter + 1) % phrases.length;
 };
